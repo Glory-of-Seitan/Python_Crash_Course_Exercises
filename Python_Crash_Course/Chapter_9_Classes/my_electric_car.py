10e7a873c0d42958ccbc529041abaadd134a5aa6
Glory-of-Seitan <john.chris.corrigan@gmail.com>
2023-01-23 20:16:19 -0600

    fiddling with subclass exercises

--- Python_Crash_Course/Chapter_9_Classes/my_electric_car.py
+++ Python_Crash_Course/Chapter_9_Classes/my_electric_car.py
@@ -2,10 +2,10 @@ from electric_car import *
 
 my_tesla = ElectricCar("tesla", "model S", "2021", 75)
 print(f"\n{my_tesla.get_desc_name()}")
-my_tesla.odometer_reading = 320
+# my_tesla.odometer_reading = 320
 my_tesla.read_odometer()
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
+# my_tesla.battery.upgrade_battery()
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
